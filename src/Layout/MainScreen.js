0e03eeaae4c9190ecde94efe6cb588d8afbcd569
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2023-01-24 23:05:43 +0700

    UI AuthenticationCodeScreen

--- src/Layout/MainScreen.js
+++ src/Layout/MainScreen.js
@@ -3,8 +3,8 @@ import { Text, View, Icon, Image, Button } from '@Components';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import alert from '@Alert';
 import Global from '@Global';
-import SignInScreen from './Authentication/SignInScreen';
 import SignUpScreen from './Authentication/SignUpScreen';
+import AuthenticationCodeScreen from './Authentication/AuthenticationCodeScreen';
 import { useTranslation } from 'react-i18next';
 
 const MainScreen = () => {
