ea13827209bb2e61d5c97f56fa075e79e22e22c1
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2023-04-23 10:59:41 +0700

    TermOfServiceScreen

---
+++ src/Assets/Html/TermOfService/term_of_service_vi.js
@@ -0,0 +1,93 @@
+export default `
+<p>Vui lòng đọc kỹ các điều khoản và điều kiện này trước khi sử dụng Dịch vụ của chúng tôi.</p>
+<h1>Giải thích và Định nghĩa</h1>
+<h2>Giải thích</h2>
+<p>Các từ mà chữ cái đầu tiên được viết hoa có nghĩa được xác định theo các điều kiện sau. Các định nghĩa sau đây sẽ có cùng ý nghĩa bất kể chúng xuất hiện ở dạng số ít hay số nhiều.</p>
+<h2>Định nghĩa</h2>
+<p>Vì mục đích của các Điều khoản và Điều kiện này:</p>
+<ul>
+<li>
+<p><strong>Ứng dụng</strong> có nghĩa là chương trình phần mềm do Công ty cung cấp được Bạn tải xuống trên bất kỳ thiết bị điện tử nào, có tên là Job Here.</p>
+</li>
+<li>
+<p><strong>Liên kết</strong> có nghĩa là một thực thể kiểm soát, chịu sự kiểm soát hoặc dưới sự kiểm soát chung với một bên, trong đó &quot;kiểm soát&quot; có nghĩa là sở hữu từ 50% trở lên cổ phần, lợi ích vốn cổ phần hoặc chứng khoán khác có quyền bỏ phiếu bầu giám đốc hoặc cơ quan quản lý khác.</p>
+</li>
+<li>
+<p><strong>Quốc gia</strong> đề cập đến:  Việt Nam.</p>
+</li>
+<li>
+<p><strong>Công ty</strong> (được gọi là "Chúng tôi" hoặc "Của chúng tôi" trong Thỏa thuận này) đề cập đến Job Here.</p>
+</li>
+<li>
+<p><strong>Thiết bị</strong> có nghĩa là bất kỳ thiết bị nào có thể truy cập Dịch vụ, chẳng hạn như máy tính, điện thoại di động hoặc máy tính bảng kỹ thuật số.</p>
+</li>
+<li>
+<p><strong>Dịch vụ</strong> đề cập đến Ứng dụng hoặc Trang web hoặc cả hai.</p>
+</li>
+<li>
+<p><strong>Điều khoản và Điều kiện</strong> (còn được gọi là "Điều khoản") có nghĩa là các Điều khoản và Điều kiện này tạo thành toàn bộ thỏa thuận giữa Bạn và Công ty về việc sử dụng Dịch vụ.</p>
+</li>
+<li>
+
+<p><strong>Dịch vụ truyền thông xã hội của bên thứ ba</strong> có nghĩa là bất kỳ dịch vụ hoặc nội dung nào (bao gồm dữ liệu, thông tin, sản phẩm hoặc dịch vụ) do bên thứ ba cung cấp có thể được hiển thị, bao gồm hoặc cung cấp bởi Dịch vụ.</p>
+</li>
+<li>
+<p><strong>Trang Web</strong> đề cập đến Job Here, có thể truy cập từ <a href="https://jobhere.tech/" rel="external nofollow noopener" target="_blank">https://jobhere.tech/</a></p>
+</li>
+<li>
+<p><strong>Bạn</strong> có nghĩa là cá nhân truy cập hoặc sử dụng Dịch vụ, hoặc công ty hoặc pháp nhân khác thay mặt cho cá nhân đó đang truy cập hoặc sử dụng Dịch vụ, nếu có.</p>
+</li>
+</ul>
+<h1>Nhìn nhận</h1>
+<p>Đây là các Điều khoản và Điều kiện chi phối việc sử dụng Dịch vụ này và thỏa thuận hoạt động giữa Bạn và Công ty. Các Điều khoản và Điều kiện này quy định các quyền và nghĩa vụ của tất cả người dùng liên quan đến việc sử dụng Dịch vụ.</p>
+<p>Việc bạn truy cập và sử dụng Dịch vụ tùy thuộc vào việc Bạn chấp nhận và tuân thủ các Điều khoản và Điều kiện này. Các Điều khoản và Điều kiện này áp dụng cho tất cả khách truy cập, người dùng và những người khác truy cập hoặc sử dụng Dịch vụ.</p>
+<p>Bằng cách truy cập hoặc sử dụng Dịch vụ, Bạn đồng ý bị ràng buộc bởi các Điều khoản và Điều kiện này. Nếu Bạn không đồng ý với bất kỳ phần nào của các Điều khoản và Điều kiện này thì Bạn không thể truy cập Dịch vụ.</p>
+<p>Bạn tuyên bố rằng bạn trên 18 tuổi. Công ty không cho phép những người dưới 18 tuổi sử dụng Dịch vụ.</p>
+<p>Việc bạn truy cập và sử dụng Dịch vụ cũng tùy thuộc vào việc Bạn chấp nhận và tuân thủ Chính sách quyền riêng tư của Công ty. Chính sách quyền riêng tư của chúng tôi mô tả các chính sách và thủ tục của chúng tôi về việc thu thập, sử dụng và tiết lộ thông tin cá nhân của bạn khi bạn sử dụng Ứng dụng hoặc Trang web và cho bạn biết về quyền riêng tư của bạn và cách luật bảo vệ bạn. Vui lòng đọc kỹ Chính sách bảo mật của chúng tôi trước khi sử dụng Dịch vụ của chúng tôi.</p>
+<h1>Liên kết đến các trang web khác</h1>
+<p>Dịch vụ của chúng tôi có thể chứa các liên kết đến các trang web hoặc dịch vụ của bên thứ ba không thuộc sở hữu hoặc kiểm soát của Công ty.</p>
+<p>Công ty không có quyền kiểm soát và không chịu trách nhiệm về nội dung, chính sách quyền riêng tư hoặc thông lệ của bất kỳ trang web hoặc dịch vụ của bên thứ ba nào. Bạn cũng thừa nhận và đồng ý rằng Công ty sẽ không chịu trách nhiệm hoặc trách nhiệm pháp lý, trực tiếp hoặc gián tiếp, đối với bất kỳ thiệt hại hoặc tổn thất nào gây ra hoặc bị cáo buộc là do hoặc liên quan đến việc sử dụng hoặc phụ thuộc vào bất kỳ nội dung, hàng hóa hoặc dịch vụ nào có sẵn trên hoặc thông qua bất kỳ trang web hoặc dịch vụ nào như vậy.</p>
+<p>Chúng tôi thực sự khuyên Bạn nên đọc các điều khoản và điều kiện cũng như chính sách quyền riêng tư của bất kỳ trang web hoặc dịch vụ bên thứ ba nào mà Bạn truy cập.</p>
+<h1>Chấm dứt</h1>
+<p>Chúng tôi có thể chấm dứt hoặc đình chỉ quyền truy cập của Bạn ngay lập tức mà không cần thông báo trước hoặc chịu trách nhiệm pháp lý vì bất kỳ lý do gì, bao gồm nhưng không giới hạn nếu Bạn vi phạm các Điều khoản và Điều kiện này.</p>
+<p>Khi chấm dứt, quyền sử dụng Dịch vụ của bạn sẽ chấm dứt ngay lập tức.</p>
+<h1>Giới hạn Trách nhiệm</h1>
+<p>Bất kể mọi thiệt hại mà Bạn có thể phải chịu, toàn bộ trách nhiệm pháp lý của Công ty và bất kỳ nhà cung cấp nào của Công ty theo bất kỳ điều khoản nào của Điều khoản này và biện pháp khắc phục độc quyền của Bạn đối với tất cả những điều đã nói ở trên sẽ được giới hạn ở số tiền Bạn thực trả thông qua Dịch vụ hoặc 100 USD nếu Bạn chưa mua bất cứ thứ gì thông qua Dịch vụ.</p>
+<p>Trong phạm vi tối đa được pháp luật hiện hành cho phép, trong mọi trường hợp, Công ty hoặc nhà cung cấp của Công ty sẽ không chịu trách nhiệm pháp lý cho bất kỳ thiệt hại đặc biệt, ngẫu nhiên, gián tiếp hoặc do hậu quả nào (bao gồm nhưng không giới hạn ở thiệt hại do mất lợi nhuận, mất dữ liệu hoặc thông tin khác, vì lý do gián đoạn kinh doanh, vì thương tích cá nhân, mất quyền riêng tư phát sinh từ hoặc theo bất kỳ cách nào liên quan đến việc sử dụng hoặc không thể sử dụng Dịch vụ, phần mềm của bên thứ ba và/hoặc phần cứng của bên thứ ba được sử dụng với Dịch vụ, hoặc mặt khác liên quan đến bất kỳ điều khoản nào của Điều khoản này), ngay cả khi Công ty hoặc bất kỳ nhà cung cấp nào đã được thông báo về khả năng xảy ra những thiệt hại đó và ngay cả khi biện pháp khắc phục không đạt được mục đích thiết yếu của nó.</p>
+<p>Một số khu vực không cho phép loại trừ các bảo đảm ngụ ý hoặc giới hạn trách nhiệm pháp lý đối với các thiệt hại ngẫu nhiên hoặc do hậu quả, điều đó có nghĩa là một số giới hạn trên có thể không được áp dụng. Ở những tiểu bang này, trách nhiệm pháp lý của mỗi bên sẽ được giới hạn trong phạm vi lớn nhất được pháp luật cho phép.</p>
+<h1>Tuyên bố miễn trừ trách nhiệm "NGUYÊN TRẠNG" và "NHƯ SẴN CÓ"</h1>
+<p>Dịch vụ được cung cấp cho Bạn "NGUYÊN TRẠNG" và "NHƯ CÓ SẴN" và với tất cả lỗi và khiếm khuyết mà không có bảo hành dưới bất kỳ hình thức nào. Trong phạm vi tối đa được phép theo luật hiện hành, Công ty, thay mặt cho chính mình và thay mặt cho các Chi nhánh của mình cũng như các nhà cấp phép và nhà cung cấp dịch vụ tương ứng của họ, từ chối rõ ràng mọi bảo đảm, dù rõ ràng, ngụ ý, theo luật định hay cách khác, đối với Dịch vụ, bao gồm tất cả các bảo đảm ngụ ý về khả năng bán được, tính phù hợp cho một mục đích cụ thể, quyền sở hữu và việc không vi phạm cũng như các bảo đảm có thể phát sinh trong quá trình giao dịch, quá trình thực hiện, sử dụng hoặc thông lệ thương mại. Không giới hạn ở những điều đã nói ở trên, Công ty không đưa ra bảo đảm hay cam kết và không tuyên bố dưới bất kỳ hình thức nào rằng Dịch vụ sẽ đáp ứng các yêu cầu của Bạn, đạt được bất kỳ kết quả dự kiến ​​nào.</p>
+<p>Dịch vụ được cung cấp cho Bạn "NGUYÊN TRẠNG" và "NHƯ CÓ SẴN" và với tất cả lỗi và khiếm khuyết mà không có bảo hành dưới bất kỳ hình thức nào. Trong phạm vi tối đa được phép theo luật hiện hành, Công ty, thay mặt cho chính mình và thay mặt cho các Chi nhánh của mình cũng như các nhà cấp phép và nhà cung cấp dịch vụ tương ứng của họ, từ chối rõ ràng mọi bảo đảm, dù rõ ràng, ngụ ý, theo luật định hay cách khác, đối với Dịch vụ, bao gồm tất cả các bảo đảm ngụ ý về khả năng bán được, tính phù hợp cho một mục đích cụ thể, quyền sở hữu và việc không vi phạm cũng như các bảo đảm có thể phát sinh trong quá trình giao dịch, quá trình thực hiện, sử dụng hoặc thông lệ thương mại. Không giới hạn ở những điều đã nói ở trên, Công ty không đưa ra bảo đảm hay cam kết và không tuyên bố dưới bất kỳ hình thức nào rằng Dịch vụ sẽ đáp ứng các yêu cầu của Bạn, đạt được bất kỳ kết quả dự kiến ​​nào.</p>
+<p>Một số khu vực pháp lý không cho phép loại trừ một số loại bảo đảm hoặc giới hạn đối với các quyền theo luật hiện hành của người tiêu dùng, vì vậy một số hoặc tất cả các loại trừ và giới hạn trên có thể không áp dụng cho Bạn. Nhưng trong trường hợp như vậy, các loại trừ và giới hạn được nêu trong phần này sẽ được áp dụng trong phạm vi lớn nhất có thể thi hành theo luật hiện hành.</p>
+<h1>Luật chi phối</h1>
+<p>Luật pháp của Quốc gia, ngoại trừ các xung đột về quy tắc luật pháp, sẽ điều chỉnh Điều khoản này và việc Bạn sử dụng Dịch vụ. Việc bạn sử dụng Ứng dụng cũng có thể phải tuân theo luật địa phương, khu vực, quốc gia hoặc quốc tế khác.</p>
+<h1>Giải quyết tranh chấp</h1>
+<p>Nếu Bạn có bất kỳ lo ngại hoặc tranh chấp nào về Dịch vụ, trước tiên, Bạn đồng ý cố gắng giải quyết tranh chấp một cách không chính thức bằng cách liên hệ với Công ty.</p>
+<h1>Đối với người dùng ở Liên minh Châu Âu (EU)</h1>
+<p>Nếu Bạn là người tiêu dùng của Liên minh Châu Âu, bạn sẽ được hưởng lợi từ bất kỳ quy định bắt buộc nào của luật pháp của quốc gia nơi bạn cư trú.</p>
+<h1>Tuân thủ pháp luật Hoa Kỳ</h1>
+<p>Bạn tuyên bố và đảm bảo rằng (i) Bạn không sống ở một quốc gia chịu lệnh cấm vận của chính phủ Hoa Kỳ, hoặc đã được chính phủ Hoa Kỳ chỉ định là quốc gia "hỗ trợ khủng bố" và (ii) Bạn không được liệt kê trong bất kỳ danh sách các bên bị cấm hoặc hạn chế nào của chính phủ Hoa Kỳ.</p>
+<h1>Mức độ nghiêm trọng và Miễn trừ</h1>
+<h2>Mức độ nghiêm trọng</h2>
+<p>Nếu bất kỳ điều khoản nào trong các Điều khoản này được coi là không thể thi hành hoặc không hợp lệ, thì điều khoản đó sẽ được thay đổi và giải thích để hoàn thành các mục tiêu của điều khoản đó trong phạm vi lớn nhất có thể theo luật hiện hành và các điều khoản còn lại sẽ tiếp tục có hiệu lực đầy đủ.</p>
+<h2>Miễn trừ</h2>
+<p>Trừ khi được quy định ở đây, việc không thực hiện quyền hoặc yêu cầu thực hiện nghĩa vụ theo các Điều khoản này sẽ không ảnh hưởng đến khả năng của một bên trong việc thực hiện quyền đó hoặc yêu cầu thực hiện như vậy vào bất kỳ thời điểm nào sau đó cũng như việc từ bỏ vi phạm sẽ cấu thành từ bỏ bất kỳ vi phạm nào sau đó.</p>
+<h1>Biên Phiên Dịch</h1>
+<p>Các Điều khoản và Điều kiện này có thể đã được dịch nếu Chúng tôi cung cấp chúng cho Bạn trên Dịch vụ của chúng tôi. Bạn đồng ý rằng văn bản gốc bằng tiếng Anh sẽ được ưu tiên áp dụng trong trường hợp có tranh chấp.</p>
+<h1>Thay đổi đối với các Điều khoản và Điều kiện này</h1>
+<p>Chúng tôi bảo lưu quyền, theo quyết định riêng của mình, sửa đổi hoặc thay thế các Điều khoản này bất cứ lúc nào. Nếu bản sửa đổi là quan trọng, Chúng tôi sẽ nỗ lực hợp lý để thông báo trước ít nhất 30 ngày trước khi bất kỳ điều khoản mới nào có hiệu lực. Điều gì cấu thành một thay đổi quan trọng sẽ được xác định theo quyết định riêng của Chúng tôi.</p>
+<p>Bằng cách tiếp tục truy cập hoặc sử dụng Dịch vụ của chúng tôi sau khi những sửa đổi đó có hiệu lực, Bạn đồng ý bị ràng buộc bởi các điều khoản sửa đổi. Nếu Bạn không đồng ý với các điều khoản mới, toàn bộ hoặc một phần, vui lòng ngừng sử dụng trang web và Dịch vụ.</p>
+<h1>Liên hệ với Chúng tôi</h1>
+<p>Nếu bạn có bất kỳ câu hỏi nào về các Điều khoản và Điều kiện này, Bạn có thể liên hệ với chúng tôi:</p>
+<ul>
+<li>
+<p>Qua email: jobhere.contact@gmail.com</p>
+</li>
+<li>
+<p>Bằng cách truy cập trang này trên trang web của chúng tôi: <a href="https://jobhere.tech/" rel="external nofollow noopener" target="_blank">https://jobhere.tech/</a></p>
+</li>
+<li>
+<p>Qua số điện thoại: 037 999 9999</p>
+</li>
+</ul>
+`;
--- src/Screen/Common/TermOfServiceScreen.js
+++ src/Screen/Common/TermOfServiceScreen.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { View, Text, Common } from '@Components';
+import { useWindowDimensions, ScrollView } from 'react-native';
+import { View, Common } from '@Components';
 import { goBack } from '@NavigationAction';
 import { useTranslation } from 'react-i18next';
+import term_of_service_en from '@Assets/Html/TermOfService/term_of_service_en';
+import term_of_service_vi from '@Assets/Html/TermOfService/term_of_service_vi';
 
 const TermOfServiceScreen = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const { width } = useWindowDimensions();
 
   return (
     <View.Col>
       <Common.Header title={t('jh.termOfService')} actionLeft={goBack} />
-      <Text.Body secondary>TermOfServiceScreen</Text.Body>
+      <ScrollView style={{ paddingHorizontal: 10, marginBottom: 40 }}>
+        <Common.RenderHTMLJobHere
+          contentWidth={width}
+          source={{
+            html:
+              i18n.language === 'en' ? term_of_service_en : term_of_service_vi
+          }}
+          tagsStyles={{
+            ul: {
+              listStyleType: 'none',
+              padding: 0,
+              margin: 0
+            }
+          }}
+        />
+      </ScrollView>
     </View.Col>
   );
 };
